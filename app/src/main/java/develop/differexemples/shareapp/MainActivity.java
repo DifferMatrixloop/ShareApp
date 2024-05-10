5f58959d1b5967a4bbbf56cf3f6919c09d5993e9
DifferF <differ.free@gmail.com>
2024-05-10 13:22:28 +0300

    Initial commit

---
+++ app/src/main/java/develop/differexemples/shareapp/MainActivity.java
@@ -0,0 +1,40 @@
+package develop.differexemples.shareapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    // Метод для отправки ссылки на приложение
+    public void shareApp(View view) {
+        String appPackageName = getPackageName(); // Получаем имя пакета текущего приложения
+        String shareUrl = "https://play.google.com/store/apps/details?id=" + appPackageName;
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Попробуйте наше приложение: " + shareUrl);
+        startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"));
+    }
+
+   
+    // Email приглашения:
+    // Позвольте пользователям отправлять приглашения через электронную почту, что также дает возможность персонализировать сообщения.
+    public void sendEmailInvite(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Попробуйте это приложение");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Я хочу порекомендовать это приложение...");
+        startActivity(Intent.createChooser(emailIntent, "Отправить приглашение по Email"));
+    }
+
+}
